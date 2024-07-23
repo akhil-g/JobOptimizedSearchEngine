870d34057e546d9e3f5a329304cae25edace2aac
akhil-g <50015300+akhil-g@users.noreply.github.com>
2024-07-23 11:22:16 -0400

    add favicon

--- JobOptimizedSearchEngine/settings.py
+++ JobOptimizedSearchEngine/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-1kaqbrebn@fu3x2wucrdqcysjptn%d+cc1bu_b&e5)t17pq8%$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -122,3 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [BASE_DIR / 'static']
--- myapp/urls.py
+++ myapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.contrib.staticfiles.storage import staticfiles_storage
+from django.views.generic.base import RedirectView
 from . import views
 
 urlpatterns = [
     path('', views.get_name, name='get_name'),
     path('download/<str:filename>/', views.download_file, name='download_file'),
+    path('favicon.ico', RedirectView.as_view(
+        url=staticfiles_storage.url('images/favicon.ico')))
 ]
